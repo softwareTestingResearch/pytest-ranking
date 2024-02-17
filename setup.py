01c7d95556c151e622740b21415898c6057d8b71
softwareTestingResearch <testingresearch4all@gmail.com>
2024-02-16 23:27:41 -0500

    0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name='pytest-ranking',
-    version='0.1.0',
+    version='0.2.0',
     author='softwareTestingResearch',
     author_email='testingresearch4all@gmail.com',
     maintainer='softwareTestingResearch',
